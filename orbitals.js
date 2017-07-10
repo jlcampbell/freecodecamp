17e5b23dc8e8e001e8b44fa30bca7030e9b23ab6
jlcampbell <jlcampbell@users.noreply.github.com>
2017-07-10 15:37:43 -0400

    code works

--- orbitals.js
+++ orbitals.js
@@ -1,1 +1,16 @@
-//
+function orbitalPeriod(arr) {
+  var GM = 398600.4418;
+  var earthRadius = 6367.4447;
+  var answer= [];
+  for (var i=0; i<arr.length; i++){
+    var flyingObject = arr[i];
+    var a = flyingObject.avgAlt+earthRadius;
+    delete flyingObject.avgAlt;
+    flyingObject.orbitalPeriod = Math.round(2*Math.PI*Math.pow((Math.pow(a, 3)/GM), 0.5));
+    answer.push(flyingObject);
+  }
+  return answer;
+}
+
+
+orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
