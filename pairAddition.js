367bb4a08795a18c4bccac80bc8be070261961e9
jlcampbell <jlcampbell@users.noreply.github.com>
2017-07-11 15:28:01 -0400

    refactored

--- pairAddition.js
+++ pairAddition.js
@@ -1,15 +1,14 @@
 function pairwise(arr, arg) {
-  var numbers = arr;
   var count = 0;
-  var uniqueNumbers = numbers.reduce(function(allNumbers, number){
+  var uniqueNumbers = arr.reduce(function(allNumbers, number){
     //number already in accumulator
     if (allNumbers.indexOf(number)>-1) {
     }
     //number's pair is one of the
-    else if (numbers.indexOf(arg-number)>-1) {
+    else if (arr.indexOf(arg-number)>-1) {
       allNumbers.push(number);
       allNumbers.push(arg-number);
-      count += numbers.indexOf(number)+numbers.indexOf(arg-number);
+      count += arr.indexOf(number)+arr.indexOf(arg-number);
     }
     return allNumbers;
   }, []);
