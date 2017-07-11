87e0a5de487faf80e942f78d5b290a8da9354def
jlcampbell <jlcampbell@users.noreply.github.com>
2017-07-11 15:16:01 -0400

    works but needs to be refactored

--- pairAddition.js
+++ pairAddition.js
@@ -1,5 +1,20 @@
 function pairwise(arr, arg) {
-  return arg;
+  var numbers = arr;
+  var count = 0;
+  var uniqueNumbers = numbers.reduce(function(allNumbers, number){
+    //number already in accumulator
+    if (allNumbers.indexOf(number)>-1) {
+    }
+    //number's pair is one of the
+    else if (numbers.indexOf(arg-number)>-1) {
+      allNumbers.push(number);
+      allNumbers.push(arg-number);
+      count += numbers.indexOf(number)+numbers.indexOf(arg-number);
+    }
+    return allNumbers;
+  }, []);
+
+  return count;
 }
 
 pairwise([1,4,2,3,0,5], 7);
