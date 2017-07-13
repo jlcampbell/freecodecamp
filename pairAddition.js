66f24a581131e8f759bb106c153b70d473e94b85
jlcampbell <jlcampbell@users.noreply.github.com>
2017-07-13 15:15:30 -0400

    getting closer

--- pairAddition.js
+++ pairAddition.js
@@ -1,19 +1,40 @@
+//need to take in array and arg
+//need to look at each element of array
+//see if that element's pair is in array
+//see if that element has already been paired
+//see if that element's pair has already been paired
+
+//an object with {argument:index}. when a value gets used it is removed from
+//the object
+
 function pairwise(arr, arg) {
   var count = 0;
-  var uniqueNumbers = arr.reduce(function(allNumbers, number){
-    //number already in accumulator
-    if (allNumbers.indexOf(number)>-1) {
+  var objOfNumbers = arr.reduce(function(accum, number, numberIndex){
+    if (accum.hasOwnProperty(number)){
+        accum[number].push(numberIndex);
     }
-    //number's pair is one of the
-    else if (arr.indexOf(arg-number)>-1) {
-      allNumbers.push(number);
-      allNumbers.push(arg-number);
-      count += arr.indexOf(number)+arr.indexOf(arg-number);
+    else {
+        accum[number]=[numberIndex];
     }
-    return allNumbers;
-  }, []);
+    return accum;
+  }, {});
+
+  for (i=0; i<arr.length; i++){
+
+      if (arr[i] in objOfNumbers){
+          console.log(arr[i]+"in objOfNumbers");
+          if (arg-arr[i] in objOfNumbers){
+              count += objOfNumbers[arr[i][0]];
+              count += objOfNumbers[(arg-arr[i])[0]];
+              delete objOfNumbers[arr[i][0]];
+              delete objOfNumbers[(arg-arr[i])[0]];
+              console.log(objOfNumbers);
+          }
+      }
+
+  }
 
   return count;
 }
 
-pairwise([1,4,2,3,0,5], 7);
+pairwise([1,4,4,2,3,0,5], 7);
